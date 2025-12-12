69f8109f9b0ab9644ef8b75bbb1f853d3a8edf9c
tanh <trtanh1612@gmail.com>
2025-12-13 01:24:05 +0700

    5. Dashboard UI & Brand, Category APIs

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,11 +1,24 @@
 <?php
 
 use App\Http\Controllers\admin\AuthController;
+use App\Http\Controllers\admin\BrandController;
+use App\Http\Controllers\admin\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/admin/login', [AuthController::class, 'authenticate']);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    // Route::get('categories', [CategoryController::class, 'index']);
+    // Route::get('categories/{id}', [CategoryController::class, 'show']);
+    // Route::post('categories', [CategoryController::class, 'store']);
+    // Route::put('categories/{id}', [CategoryController::class, 'update']);
+    // Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
+
+    Route::resource('categories', CategoryController::class);
+    Route::resource('brands', BrandController::class);
+});
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,18 +1,60 @@
-import { useContext } from "react";
+// import { useContext } from "react";
+// import { AdminAuthContext } from "../../context";
+import Sidebar from "../common/Sidebar";
 import Layout from "../common/Layout";
-import { AdminAuthContext } from "../../context";
 
 const Dashboard = () => {
-  const { logout } = useContext(AdminAuthContext);
+  // const { logout } = useContext(AdminAuthContext);
   return (
-    <>
-      <Layout>
-        <h1>Dashboard</h1>
-        <button className="btn btn-danger" onClick={logout}>
-          Logout
-        </button>
-      </Layout>
-    </>
+    <Layout>
+      <div className="container-md">
+        <div className="row">
+          <div className="d-flex justify-content-between mt-5 pb-3">
+            <h4 className="h4 pb-0 mb-0">Dashboard</h4>
+          </div>
+          <div className="col-md-3">
+            <Sidebar />
+          </div>
+          <div className="col-md-9">
+            <div className="row">
+              <div className="col-md-4">
+                <div className="card shadow">
+                  <div className="card-body">
+                    <h2>0</h2>
+                    <span>Users</span>
+                  </div>
+                  <div className="card-footer">
+                    <a href="#">View Users</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="card shadow">
+                  <div className="card-body">
+                    <h2>0</h2>
+                    <span>Orders</span>
+                  </div>
+                  <div className="card-footer">
+                    <a href="#">View Orders</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="card shadow">
+                  <div className="card-body">
+                    <h2>0</h2>
+                    <span>Products</span>
+                  </div>
+                  <div className="card-footer">
+                    <a href="#">View Products</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
   );
 };
 
