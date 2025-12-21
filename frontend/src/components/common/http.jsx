a78be2555626b3a9671b8036b3055ee11f518e75
tanh <trtanh1612@gmail.com>
2025-12-22 00:40:04 +0700

    25.1

--- frontend/src/components/common/http.jsx
+++ frontend/src/components/common/http.jsx
@@ -1,11 +1,13 @@
 export const apiUrl = "http://localhost:8000/api";
 
 export const adminToken = () => {
-  const data = JSON.parse(localStorage.getItem("adminInfo"));
-  return data.token;
+    const json = localStorage.getItem("adminInfo");
+    const data = json ? JSON.parse(json) : null;
+    return data?.token; // Trả về token hoặc undefined, KHÔNG báo lỗi crash
 };
 
 export const userToken = () => {
-  const data = JSON.parse(localStorage.getItem("userInfo"));
-  return data.token;
+    const json = localStorage.getItem("userInfo");
+    const data = json ? JSON.parse(json) : null;
+    return data?.token; // Trả về token hoặc undefined, KHÔNG báo lỗi crash
 };
